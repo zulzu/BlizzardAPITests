34e52b3abf31756be95770d68a8a99ec9568e7f9
Andras Pal <andr4s.pal@gmail.com>
2021-01-21 10:59:27 +0000

    Initial Commit

---
+++ BlizzardAPITestsWithOAuth2/BlizzardAPITestsWithOAuth2App.swift
@@ -0,0 +1,17 @@
+//
+//  BlizzardAPITestsWithOAuth2App.swift
+//  BlizzardAPITestsWithOAuth2
+//
+//  Created by Andras Pal on 21/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct BlizzardAPITestsWithOAuth2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
